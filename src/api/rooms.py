1786b7f2fba2a41bc19f9cdb1814a610386d224e
eushakova1 <eushakova1@eqvatech.ru>
2025-12-25 14:38:55 +0300

    upd pacth api for room

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -24,7 +24,7 @@ async def get_room_by_id(hotel_id: int, room_id: int):
 
 
 @router.post("/{hotel_id}/rooms")
-async def create_hotel(
+async def create_room(
     hotel_id: int,
     room_data: RoomCreateRequest = Body(),
 ):
@@ -36,7 +36,7 @@ async def create_hotel(
 
 
 @router.put("/{hotel_id}/rooms/{room_id}")
-async def edit_hotel(hotel_id: int, room_id: int, room_data: RoomCreateRequest):
+async def edit_room(hotel_id: int, room_id: int, room_data: RoomCreateRequest):
     _room_data = RoomCreate(hotel_id=hotel_id, **room_data.model_dump())
     async with async_session_maker() as session:
         await RoomsRepository(session).update(_room_data, id=room_id)
@@ -47,12 +47,12 @@ async def edit_hotel(hotel_id: int, room_id: int, room_data: RoomCreateRequest):
 @router.patch(
     "/{hotel_id}/rooms/{room_id}",
 )
-async def partially_edit_hotel(
+async def partially_edit_room(
     hotel_id: int,
     room_id: int,
     room_data: RoomPatchRequest,
 ):
-    _room_data = RoomPatch(hotel_id=hotel_id, **room_data.model_dump())
+    _room_data = RoomPatch(hotel_id=hotel_id, **room_data.model_dump(exclude_unset=True))
     async with async_session_maker() as session:
         await RoomsRepository(session).update(_room_data, is_patch=True, id=room_id)
         await session.commit()
